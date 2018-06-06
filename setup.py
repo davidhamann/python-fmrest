1ef78c193ab5f64ebb658187d4f36436159a6faa
David Hamann <dh@davidhamannmedia.com>
2018-06-06 23:09:50 +0200

    Preparation for PyPI, v1.0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r") as ld:
+    long_description = ld.read()
+
 setup(
     name='python-fmrest',
-    version='1.0.1',
+    version='1.0.2',
     author='David Hamann',
     author_email='dh@davidhamann.de',
+    description='python-fmrest is a wrapper around the FileMaker Data API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/davidhamann/python-fmrest',
     packages=['fmrest'],
     include_package_data=True,
-    install_requires=['requests']
+    install_requires=['requests'],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    )
 )
